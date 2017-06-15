4ede8721ab877b9024926643ed05942bd1606f70
Zaki <bakshizaki@gmail.com>
2017-06-15 10:04:54 -0400

    Updated Unit tests

--- SEC/src/EngineeringEducator.java
+++ SEC/src/EngineeringEducator.java
@@ -1,5 +1,3 @@
-package sec;
-
 import java.awt.image.BufferedImage;
 import java.io.File;
 
--- SEC/src/SecWindow.java
+++ SEC/src/SecWindow.java
@@ -1,5 +1,3 @@
-package sec;
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
