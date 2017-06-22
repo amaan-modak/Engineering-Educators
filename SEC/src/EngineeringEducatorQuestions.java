79f1538737a75891870599eba7ef1d3ba1f4a2d5
Archita Pathak <architap@buffalo.edu>
2017-06-22 10:32:28 -0400

    Added method to get total number of questions

--- SEC/src/EngineeringEducatorQuestions.java
+++ SEC/src/EngineeringEducatorQuestions.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class EngineeringEducatorQuestions {
-	ArrayList<String> displayedQuestionFolders = new ArrayList<String>();
 	/*
 	 * FOLDER RANDOM SELECTION Method to randomly select a question folder
 	 * Input: 1. EngineeringEducator class object 2. Path of the directory
@@ -17,15 +16,26 @@ public class EngineeringEducatorQuestions {
 			Random rand = new Random();
 			
 			String folder = quesfolders[rand.nextInt(quesfolders.length)];
-			if(!displayedQuestionFolders.contains(folder)){
-				displayedQuestionFolders.add(folder);
-				questionPath = folderPath + folder;
-			}
+			questionPath = folderPath + folder;
 			
 		}catch(Exception ex){
 			ex.printStackTrace();
 		}
 		
 		return questionPath;
+	}
+	
+	public int getTotalQuestions(String folderPath){
+		int num = 0;
+		try{
+			File dir = new File(folderPath);
+			String[] quesfolders = dir.list();
+			num = quesfolders.length;
+			
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+		
+		return num;
 	}
 }
