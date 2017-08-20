81c30dda4379c1fc1d2afacbc613b317de1dfb71
ManishKasireddy <manishka@buffalo.edu>
2017-08-20 23:26:27 +0530

    Support for GitClone when questions not available locally

--- SEC/src/teamnp/eguru/MainPage.java
+++ SEC/src/teamnp/eguru/MainPage.java
@@ -16,6 +16,8 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
@@ -193,6 +195,17 @@ public class MainPage {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
+				if(!qHandleObject.questionsFolderExists){
+					//String localPath = "Git";
+					System.out.println("Yes");
+					File lp = new File("Git");
+					try {
+						qHandleObject.deleteDir(lp);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
 				frame.dispose();
 				String [] arguments = {""+score};
 				EndPage.main(arguments);
